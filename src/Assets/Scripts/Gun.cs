72dd46422f764f939da900c60f072552a5d23b1e
Duy Tran <duytran@asnet.com.vn>
2015-09-15 23:52:20 +0700

    Add gun scripting

---
+++ src/Assets/Scripts/Gun.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    // Gun type
+    public enum GunType {Semi, Burst, Auto};
+    public GunType gunType;
+
+    // The spaw object
+    public Transform spawn;
+
+    public void Shoot () {
+
+        // Create a ray with spawn position and get forward drection
+        Ray ray = new Ray (spawn.position, spawn.forward);
+        RaycastHit hit;
+
+        // Default shoot distance is 20;
+        float shootDistance = 20;
+
+        // Physic raycast with ray object and shoot distance
+        if (Physics.Raycast(ray, out hit, shootDistance)) {
+            // If hit available mark shoot distance by hit.distance
+            shootDistance = hit.distance;
+        }
+
+        // Debug
+        Debug.DrawRay(ray.origin, ray.direction * shootDistance, Color.red, 1);
+    }
+
+    public void ShootContinuous () {
+        // If gun type is auto
+        if ( gunType == GunType.Auto ) {
+            Shoot ();
+        }
+    }
+}
