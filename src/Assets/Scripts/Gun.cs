c5d687bdea871689019fed78d1f70f80377d34dc
Duy Tran <duytran@asnet.com.vn>
2015-09-20 14:52:11 +0700

    Make gun shot with audio Make gun shot with reload time Add gun type Add Shell script

--- src/Assets/Scripts/Gun.cs
+++ src/Assets/Scripts/Gun.cs
@@ -1,32 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (AudioSource))]
 public class Gun : MonoBehaviour {
 
     // Gun type
     public enum GunType {Semi, Burst, Auto};
     public GunType gunType;
+    // Round per minute
+    public float rpm;
 
-    // The spaw object
+
+
+    // System
+    private float secondsBetweenShots;
+    private float nextPossibleShootTime;
+
+    // Components
     public Transform spawn;
+    public Transform shellEjectionPoint;
+    public Rigidbody shell;
+    private AudioSource audio;
+    private LineRenderer tracer;
+
+    void Start () {
+        secondsBetweenShots = 60/rpm;
+        audio = GetComponent<AudioSource>();
+
+        if (GetComponent<LineRenderer> ()) {
+            tracer = GetComponent<LineRenderer> ();
+        }
+    }
 
     public void Shoot () {
 
-        // Create a ray with spawn position and get forward drection
-        Ray ray = new Ray (spawn.position, spawn.forward);
-        RaycastHit hit;
+        if ( CanShoot () ) {
+            // Create a ray with spawn position and get forward drection
+            Ray ray = new Ray (spawn.position, spawn.forward);
+            RaycastHit hit;
 
-        // Default shoot distance is 20;
-        float shootDistance = 20;
+            // Default shoot distance is 20;
+            float shootDistance = 20;
 
-        // Physic raycast with ray object and shoot distance
-        if (Physics.Raycast(ray, out hit, shootDistance)) {
-            // If hit available mark shoot distance by hit.distance
-            shootDistance = hit.distance;
+            // Physic raycast with ray object and shoot distance
+            if (Physics.Raycast(ray, out hit, shootDistance)) {
+                // If hit available mark shoot distance by hit.distance
+                shootDistance = hit.distance;
+            }
+
+            // Debug
+            Debug.DrawRay(ray.origin, ray.direction * shootDistance, Color.red, 1);
+
+            nextPossibleShootTime = Time.time + secondsBetweenShots;
+            audio.Play ();
+
+            if ( tracer ) {
+                StartCoroutine("RenderTracer", ray.direction * shootDistance);
+            }
+
+            Rigidbody newShell = Instantiate(shell, shellEjectionPoint.position, Quaternion.identity)as Rigidbody;
+            newShell.AddForce(shellEjectionPoint.forward * Random.Range(150f, 200f) + spawn.forward* Random.Range(-10f, 10f));
         }
-
-        // Debug
-        Debug.DrawRay(ray.origin, ray.direction * shootDistance, Color.red, 1);
     }
 
     public void ShootContinuous () {
@@ -34,5 +68,22 @@ public class Gun : MonoBehaviour {
         if ( gunType == GunType.Auto ) {
             Shoot ();
         }
+    }
+
+    private bool CanShoot () {
+        bool canShoot = true;
+
+        if ( Time.time < nextPossibleShootTime ) {
+            canShoot = false;
+        }
+        return canShoot;
+    }
+
+    IEnumerator RenderTracer (Vector3 hitPoint) {
+        tracer.enabled = true;
+        tracer.SetPosition (0, spawn.position);
+        tracer.SetPosition (1, spawn.position + hitPoint);
+        yield return null;
+        tracer.enabled = false;
     }
 }
---
+++ src/Assets/Scripts/Shell.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shell : MonoBehaviour {
+
+    private float lifeTime = 5f;
+    private Material mat;
+    private Color origialCol;
+    private float fadePercent;
+    private float deathTime;
+    private bool fading;
+
+	// Use this for initialization
+	void Start () {
+        deathTime = Time.time + lifeTime;
+        mat = GetComponent<Renderer>().material;
+        origialCol = mat.color;
+        StartCoroutine("Fade");
+	}
+
+	IEnumerator Fade () {
+        while (true) {
+            yield return new WaitForSeconds (0.2f);
+
+            if ( fading ) {
+                fadePercent += Time.deltaTime;
+                mat.color = Color.Lerp(origialCol, Color.clear, fadePercent);
+
+                if ( fadePercent >= 1 ) {
+                    Destroy(gameObject);
+                }
+            }
+            else {
+                if ( Time.time > deathTime ) {
+                    fading = true;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter (Collider c) {
+        if ( c.tag == "Ground" ) {
+            GetComponent<Rigidbody>().Sleep();
+        }
+    }
+}
